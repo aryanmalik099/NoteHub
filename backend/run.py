32d45a6bfe8d8d5ecaca31e069341b06eb109277
aryanmalik099 <utkarshmalik088@gmail.com>
2025-10-08 03:20:17 +0530

    For Deploying

--- backend/run.py
+++ backend/run.py
@@ -1,10 +1,4 @@
-# Allow running from repo root or from backend/ by handling imports flexibly
-try:
-    # When working directory is backend/
-    from app import create_app  # type: ignore
-except ImportError:
-    # When working directory is repo root
-    from backend.app import create_app  # type: ignore
+from app import create_app
 
 app = create_app()
 
